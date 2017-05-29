5aa4c50827fb15f0a3516bde24e425861f1816a1
HyunSeo Nam <auspice7g@gmail.com>
2017-05-29 13:46:05 +0900

    model implement before migrate

--- account/managers.py
+++ account/managers.py
@@ -1,31 +1,36 @@
 # models에서 정의한 class를 django 내 객체로 받도록 하는 역할
 
 from django.contrib.auth.base_user import BaseUserManager
+import random
+# from .models import UcUser
+
 
 # models에서 User
 class UcUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, login_id, name, email, password, **extra_fields):
         """
         Creates and saves a User with the given email and password.
         """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(login_id=login_id, **extra_fields)
+        user.user_id = ''.join(random.sample('0123456789',5))
+        # TODO - user의 random key id의 중복방지 추가하여야 함
+        # if UcUser.objects.filter(pk=newid).count() == 0:
+            # self.object_id = newid
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, login_id, name, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(login_id, name, email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, login_id, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(login_id, password, **extra_fields)
--- account/models.py
+++ account/models.py
@@ -8,45 +8,60 @@ from .managers import UcUserManager
 
 # (id, name, is_admin, is_active, user_id, passwd, group_id, post_id, email, description, photo, department, major, )
 
+type_list = (
+    ("defualt", "미인증"),
+    ("student", "인증"),
+    ("company", "업자"),
+)
+
 class UcUser(AbstractBaseUser, PermissionsMixin):
 
-    user_id = models.CharField(_('user id'), max_length=16, unique=True)
-    email = models.EmailField(_('email address'), unique=True)
-    name = models.CharField(_('full name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    # 고유 키
+    user_id = models.CharField(max_length=16, unique=True, primary_key=True)
 
-    # is_admin?
-    # group_id?
-    # post_id?
-    # description 간단한 자기소개
-    # photo 프로필 사진
-    # department 뭐였더라?
-    # major
+    # 로그인 id
+    login_id = models.CharField(max_length=16, unique=True)
 
-    is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+    # 실명
+    name = models.CharField(max_length=20)
+
+    # 동아리 관리자인가
+    is_admin = models.BooleanField(default=False)
+
+    # 유니스트 메일
+    email = models.EmailField(_('email address'), max_length=255, unique=True)
+
+    # 학생받았는지 메일 인증유무 혹은 업자유무
+    type = models.CharField(choices=type_list, max_length=15, null=False)
+
+    # 간략한 소개
+    description = models.CharField(max_length=255, null=False, blank=True)
+
+    # 사진
+    photo = models.ImageField(
+        width_field="width",
+        height_field="height",
+        blank=True,
+        null=True,
+    )
+
+    # 전공(선택)
+    major = models.CharField(max_length=30)
+    # 부전공(선택)
+    minor = models.CharField(max_length=30)
+
+
+
 
     objects = UcUserManager()
 
     USERNAME_FIELD = 'user_id'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name', 'email']
 
-    def get_full_name(self):
-        '''
-        Returns the first_name plus the last_name, with a space in between.
-        '''
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
 
-    def get_short_name(self):
-        '''
-        Returns the short name for the user.
-        '''
-        return self.first_name
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
+    def get_name(self):
         '''
-        Sends an email to this User.
+        Returns the name for the user.
         '''
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+        return self.name
