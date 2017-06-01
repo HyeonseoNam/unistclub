20083d726b745942283f342470f69d47de9e6cdb
KimISeob <seobew@gmail.com>
2017-06-01 15:16:34 +0900

    add image field to group and store on /media/groups/group_name/group_photo and show on group_detail

--- unistclub/urls.py
+++ unistclub/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -31,3 +33,7 @@ urlpatterns = [
     url(r'^groups/', group_main, name="group_main"),
     url(r'^group/(?P<group_id>\d+)/$', group_detail, name='group_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
