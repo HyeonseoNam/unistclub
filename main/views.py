797a0c54395c2ea796117cde5c22c5a0bfadad91
HyunSeo Nam <auspice7g@gmail.com>
2017-06-09 03:33:15 +0900

    editting

--- main/views.py
+++ main/views.py
@@ -2,8 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'main/index.html',)
-
-def home(request):
-    return render(request, 'main/home.html')
